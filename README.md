ecac207bb777719fde26d0360c4f8150b2e1165a
Aiden Pyle <apyle1@umbc.edu>
2025-03-29 12:28:28 -0400

    Implemented IBM Logo instructions

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-CHIP8 Emulator
+# CHIP8 Emulator
 
 Written by Aiden Pyle
+
+## Instructions
+
+| Bytes | Operation |
+| ---- | ----------- |
+| 00E0 | Clears the screen |
+| 1NNN | Jumps to address NNN |
+| 6XNN | Set register X to value NN |
+| 7XNN | Add value NN to register X |
+| ANNN | Set index register to NNN |
+| DXYN | Draw N bytes at (X, Y) |
+
+
+## Sources
+
+- [Guide to making a CHIP-8 emulator - Tobias Langhoff](https://tobiasvl.github.io/blog/write-a-chip-8-emulator/#font)
+- [CHIP8 Documentation - trapexit](https://github.com/trapexit/chip-8_documentation)
+- [How To Write a Computer Emulator - Marat Fayzullin](http://www.emulation.org/EMUL8/HOWTO.html)
+- [Building a CHIP-8 Emulator - Austin Morlan](https://austinmorlan.com/posts/chip8_emulator/)
+- [CHIP8 Emulator is JavaScript - loktar00](https://github.com/loktar00/chip8/blob/master/chip8.js)
+- [CHIP8 Test Suite - Timendus](https://github.com/Timendus/chip8-test-suite)
--- mychip8.c
+++ mychip8.c
@@ -2,16 +2,35 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <unistd.h>
 #include <SDL/SDL.h>
 
-int start = 0;
 uint16_t pc;
+uint16_t ipntr;
+uint8_t spntr;
 uint8_t registers[16];
-uint16_t pointer;
 uint16_t stack[16];
-uint8_t sp;
 
+
+uint8_t getNib(uint16_t hex, int index) {
+    if (index > 3) { printf("Index too large: %i", index); return 0; }
+    uint8_t val = (hex >> 4*(3-index)) % 16;
+    return val;
+}
+
+uint16_t pop() {
+    if (spntr < 0) { return 0; }
+    uint8_t val = stack[spntr];
+    stack[spntr] = 0;
+    spntr -= 1;
+}
+
+void push(uint8_t val) {
+    if (spntr >= 15) { return; }
+    spntr += 1;
+    stack[spntr] = val;
+}
 
 /* These functions are available to you to use in your code. */
 uint8_t chip8_mem_read(uint16_t addr);
@@ -30,9 +49,11 @@ void chip8_mem_reset(void);
 
 
 void chip8_init(void) {
-    pc = 0;
-
-    chip8_draw_sprite(0, 0, 0, 5);
+    pc = 0x200;
+    ipntr = 0;
+    spntr = 0;
+    memset(registers, 0, 16);
+    memset(stack, 0, 16);
 }
 
 void chip8_reset(void) {
@@ -44,15 +65,76 @@ void chip8_shutdown(void) {
 }
 
 void chip8_execute_instruction(void) {
-    if (pc > 50) { return; }
+    if (pc > 550) { return; }
     uint16_t instruction = chip8_mem_read(pc) << 8 | chip8_mem_read(pc + 1);
 
-    uint8_t op_a = instruction >> 12;
-    uint8_t op_b = (instruction >> 8) & 0x0F00;
+    uint8_t op_a = getNib(instruction, 0);
+    uint8_t op_b = getNib(instruction, 1);
+    uint8_t op_c = getNib(instruction, 2);
+    uint8_t op_d = getNib(instruction, 3);
 
+    printf("PC: %i, Instruction: 0x%X: %i, %i\n", pc, instruction, op_a, op_b);
     pc += 2;
 
-    printf("PC: %i, Instruction: %i: %i, %i\n", pc, instruction, op_a, op_b);
+    switch (op_a) {
+        case 0:
+            // Clear frame
+            if (instruction == 0x00E0) { chip8_clear_frame(); printf("Clearing\n"); }
+            // Return value
+            else if (instruction == 0x00EE) { pc = pop(); printf("Popping\n"); }
+            break;
+        case 1:
+            // Set pc to address (Jump)
+            pc = instruction & 0x0FFF;
+            printf("Jumping to 0x%X\n", pc);
+            break;
+        case 2:
+            // Call subroutine at address NNN
+            uint16_t address = instruction & 0x0FFF;
+            printf("Calling function at 0x%X\n", address);
+            push(pc);
+            pc = address;
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            // Set register Vx to value NN
+            registers[op_b] = instruction & 0x00FF;
+            printf("Set register %i\n", op_b);
+            break;
+        case 7:
+            // Add value NN to register Vx
+            registers[op_b] += instruction & 0x00FF;
+            printf("Add %i to register %i\n", instruction & 0x00FF, op_b);
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            // Set instruction pointer to value NNN
+            ipntr = instruction & 0x0FFF;
+            printf("Set i to %i\n", ipntr);
+            break;
+        case 11:
+            break;
+        case 12:
+            break;
+        case 13:
+            // Draw sprite at (Vx, Vy) of N-bytes
+            chip8_draw_sprite(ipntr, registers[op_b], registers[op_c], op_d);
+            printf("Draw sprite: %i, %i, %i, %i\n", ipntr, registers[op_b], registers[op_c], op_d);
+            break;
+        case 14:
+            break;
+        case 15:
+            break;
+    }
 
     return;
 }
+
